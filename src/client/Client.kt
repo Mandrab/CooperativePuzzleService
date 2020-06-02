67949ddb712d8dddcdf4bc9f3b04039bbe151f4b
Paolo Baldini <paolobaldini01@gmail.com>
2020-06-02 22:36:48 +0200

    trying to implement mouse position 'stream'

--- src/client/Client.kt
+++ src/client/Client.kt
@@ -1,15 +1,7 @@
 package client
 
 import io.vertx.core.AbstractVerticle
-import io.vertx.core.AsyncResult
-import io.vertx.core.Vertx
-import io.vertx.core.buffer.Buffer
 import io.vertx.core.http.HttpClient
-import io.vertx.core.http.HttpClientOptions
-import io.vertx.core.http.WebSocket
-import io.vertx.core.json.JsonObject
-import io.vertx.ext.web.client.HttpResponse
-import io.vertx.ext.web.client.WebClient
 
 
 class Client(private val port: Int) : AbstractVerticle() {
--- src/service/Gateway.kt
+++ src/service/Gateway.kt
@@ -1,8 +1,6 @@
 package service
 
 import io.vertx.core.AbstractVerticle
-import io.vertx.core.buffer.Buffer
-import io.vertx.core.http.HttpServer
 import io.vertx.core.http.HttpServerResponse
 import io.vertx.core.http.ServerWebSocket
 import io.vertx.core.json.JsonArray
@@ -13,28 +11,7 @@ import io.vertx.ext.web.handler.BodyHandler
 import kotlin.random.Random
 
 
-class Gateway(private val port: Int) : AbstractVerticle() {
-
-    override fun start() {
-        val server: HttpServer = vertx.createHttpServer()
-
-        server.webSocketHandler {
-            it.writeTextMessage("ping")
-
-            it.textMessageHandler {
-                println(it)
-            }.exceptionHandler {
-                TODO()
-            }.closeHandler {
-                TODO()
-            }
-        }.listen(port, "localhost")
-    }
-}
-
-class PuzzleService(private val localPort: Int) : AbstractVerticle() {
-    private var idEvents: Long = 0
-
+class Gateway(private val localPort: Int) : AbstractVerticle() {
     override fun start() {
         setup()
         println("[SERVICE] Ready")
@@ -42,13 +19,15 @@ class PuzzleService(private val localPort: Int) : AbstractVerticle() {
 
     private fun setup() {
         val router: Router = Router.router(vertx)
-        router.route().handler(BodyHandler.create())
-        router.get("/puzzle").handler { availablePuzzle(it) }
-        router.post("/puzzle/:puzzleID/user").handler { newPlayer(it) }
-        router.get("/puzzle/:puzzleID/tile").handler { getTiles(it) }
-        getVertx().createHttpServer()
-            .requestHandler(router) // .webSocketHandler(this::webSocketHandler)
-            //.webSocketHandler { ws: ServerWebSocket -> webSocketHandler(ws) }
+        router.apply {
+            route().handler(BodyHandler.create())
+            get("/puzzle").handler { availablePuzzle(it) }
+            get("/puzzle/:puzzleID/tile").handler { getTiles(it) }
+            post("/puzzle/:puzzleID/user").handler { newPlayer(it) }
+        }
+        vertx.createHttpServer()
+            .requestHandler(router)
+            .webSocketHandler { webSocketHandler(it) }
             .listen(localPort)
     }
 
@@ -100,51 +79,17 @@ class PuzzleService(private val localPort: Int) : AbstractVerticle() {
         response.end()
     }
 
-
-    private fun handleGetState(ctx: RoutingContext) {
-        println("[SERVICE] New GetState from ${ctx.request().absoluteURI()}")
-        val response: HttpServerResponse = ctx.response()
-        val obj = JsonObject()
-        obj.put("state", 10)
-        response.putHeader("content-type", "application/json").end(obj.encodePrettily())
-    }
-
-    private fun handleUpdateResState(ctx: RoutingContext) {
-        println("[SERVICE] NewUpdateState from ${ctx.request().absoluteURI()}")
-        val response: HttpServerResponse = ctx.response()
-        val resId: String = ctx.request().getParam("resID")
-        println("[SERVICE] resId $resId")
-        val newState: JsonObject = ctx.getBodyAsJson()
-        println("[SERVICE] newState $newState")
-        val obj = JsonObject()
-        obj.put("newState", newState)
-        obj.put("resId", resId)
-        response.putHeader("content-type", "application/json").end(obj.encodePrettily())
-    }
-
-    private fun handleNewActionStart(ctx: RoutingContext) {
-        val response: HttpServerResponse = ctx.response()
-        // physicalAsset.doCmdStartHeating();
-        val obj = JsonObject()
-        val links = JsonObject()
-        val newActionId = 13
-        links.put("href", "/api/actions/start/$newActionId")
-        obj.put("links", links)
-        response.putHeader("content-type", "application/json").end(obj.encodePrettily())
-        response.end()
-    }
-
     private fun webSocketHandler(ws: ServerWebSocket) {
-        if (ws.path() == "/api/events") {
-            getVertx().setPeriodic(1000) { id: Long? ->
-                val obj = JsonObject()
-                obj.put("event", idEvents++)
-                val buffer: Buffer = Buffer.buffer()
-                obj.writeToBuffer(buffer)
-                ws.write(buffer)
-                println("[SERVICE] sent $obj")
+        if (ws.path().matches("/puzzle\\/([A-Z]|[a-z]|[0-9])*\\/user".toRegex())) {
+            ws.textMessageHandler {
+                println(it)
+                println(JsonObject(it).getString("player"))
+            }.binaryMessageHandler {
+                println(it)
+                println(it.toJsonObject().getString("player"))
             }
         } else {
+            println("Socket connection attempt rejected")
             ws.reject()
         }
     }
